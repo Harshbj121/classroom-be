a015f22cbf7153cbc126a01100848887adccd317
harshbj121 <harshbj121@gmail.com>
2024-08-15 21:41:06 +0530

    Update

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const cors = require('cors')
-// const UserRoutes = require('./Routes/UserRoutes')
-// const ClassroomRoutes = require('./Routes/ClassroomRoutes')
-// const MONGODB_URI= process.env.MONGODB_URI
+const UserRoutes = require('./Routes/UserRoutes')
+const ClassroomRoutes = require('./Routes/ClassroomRoutes')
+const MONGODB_URI= process.env.MONGODB_URI
 const PORT = process.env.PORT
-// const defaultUser = require('./Routes/DefaultUser')
+const defaultUser = require('./Routes/DefaultUser')
 
-// mongoose.connect(MONGODB_URI)
-// .then(() => {
-//     console.log('Connected to MongoDB');
-// })
-// .catch((error) => {
-//     console.error('Error connecting to MongoDB:', error);
-// });
+mongoose.connect(MONGODB_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
 
 app.use(cors());
 app.use(express.json());
-app.get('/', (req,res)=>{
+app.get('/test', (req,res)=>{
     res.send("Hello World")
 })
-// defaultUser();
+defaultUser();
 
-// app.use("/auth", UserRoutes);
-// app.use("/api", ClassroomRoutes);
+app.use("/auth", UserRoutes);
+app.use("/api", ClassroomRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
