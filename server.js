aa05688afe819408221a7b48626dd0bd251c1d9b
harshbj121 <harshbj121@gmail.com>
2024-08-15 20:18:51 +0530

    Done

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors')
 const UserRoutes = require('./Routes/UserRoutes')
 const ClassroomRoutes = require('./Routes/ClassroomRoutes')
-const MONGODB_URI= `mongodb+srv://harshbj121:CUw6nKspkusFr2je@classroom.6tcxj.mongodb.net/?retryWrites=true&w=majority&appName=Classroom`
+const MONGODB_URI= process.env.MONGODB_URI
+const PORT = process.env.PORT
 const defaultUser = require('./Routes/DefaultUser')
 
 mongoose.connect(MONGODB_URI)
@@ -23,6 +25,6 @@ defaultUser();
 app.use("/auth", UserRoutes);
 app.use("/api", ClassroomRoutes);
 
-app.listen(5000, () => {
-    console.log('Server running on 5000')
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
 })
